fa9a79a72ddff427e0b98b3e79b073e82233f07c
Ben Foppa <benjamin.foppa@gmail.com>
2014-07-18 16:40:57 -0400

    cleanups

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,39 @@ use std::str;
 
 pub struct App {
   vao: u32,
-  vbo: u32
+  vbo: u32,
 }
 
 impl Game for App {
   fn load(&mut self) {
-    gl::ClearColor(0.0, 0.0, 0.0, 1.0);
+    let vs = compile_shader(VS_SRC, gl::VERTEX_SHADER);
+    let fs = compile_shader(FS_SRC, gl::FRAGMENT_SHADER);
+    let program = link_program(vs, fs);
+
+    unsafe {
+        // Create Vertex Array Object
+        gl::GenVertexArrays(1, &mut self.vao);
+        gl::BindVertexArray(self.vao);
+
+        // Create a Vertex Buffer Object and copy the vertex data to it
+        gl::GenBuffers(1, &mut self.vbo);
+        gl::BindBuffer(gl::ARRAY_BUFFER, self.vbo);
+        gl::BufferData(gl::ARRAY_BUFFER,
+                        (VERTEX_DATA.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                        mem::transmute(&VERTEX_DATA[0]),
+                        gl::STATIC_DRAW);
+
+        gl::UseProgram(program);
+        "out_color".with_c_str(|ptr| gl::BindFragDataLocation(program, 0, ptr));
+
+        // Specify the layout of the vertex data
+        let pos_attr = "position".with_c_str(|ptr| gl::GetAttribLocation(program, ptr) as u32);
+        gl::EnableVertexAttribArray(pos_attr);
+        gl::VertexAttribPointer(pos_attr, 3, gl::FLOAT,
+                                gl::FALSE as GLboolean, 0, ptr::null());
+
+        gl::ClearColor(0.0, 0.0, 0.0, 1.0);
+    }
   }
 
   fn render(&mut self, _:&RenderArgs) {
@@ -36,14 +63,14 @@ impl App {
   pub fn new() -> App {
     App {
       vao: 0,
-      vbo: 0
+      vbo: 0,
     }
   }
 
   pub fn destroy(&mut self) {
     unsafe {
-        gl::DeleteBuffers(1, &self.vbo);
-        gl::DeleteVertexArrays(1, &self.vao);
+      gl::DeleteBuffers(1, &self.vbo);
+      gl::DeleteVertexArrays(1, &self.vao);
     }
   }
 }
@@ -95,6 +122,7 @@ fn compile_shader(src: &str, ty: GLenum) -> GLuint {
 
 fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
     let program = gl::CreateProgram();
+
     gl::AttachShader(program, vs);
     gl::AttachShader(program, fs);
     gl::LinkProgram(program);
@@ -113,52 +141,22 @@ fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
             fail!("{}", str::from_utf8(buf.as_slice()).expect("ProgramInfoLog not valid utf8"));
         }
     }
+
     program
 }
 
 fn main() {
-  let game_iter_settings = GameIteratorSettings {
-    updates_per_second: 30,
-    max_frames_per_second: 30
-  };
-
   let mut window = GameWindowSDL2::new(
     GameWindowSettings {
       title: "playform".to_string(),
       size: [800, 600],
       fullscreen: false,
-      exit_on_esc: true
+      exit_on_esc: false,
     }
   );
 
-  let mut app = App::new();
-
-  let vs = compile_shader(VS_SRC, gl::VERTEX_SHADER);
-  let fs = compile_shader(FS_SRC, gl::FRAGMENT_SHADER);
-  let program = link_program(vs, fs);
-
-  unsafe {
-    // Create Vertex Array Object
-    gl::GenVertexArrays(1, &mut app.vao);
-    gl::BindVertexArray(app.vao);
-
-    // Create a Vertex Buffer Object and copy the vertex data to it
-    gl::GenBuffers(1, &mut app.vbo);
-    gl::BindBuffer(gl::ARRAY_BUFFER, app.vbo);
-    gl::BufferData(gl::ARRAY_BUFFER,
-                    (VERTEX_DATA.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-                    mem::transmute(&VERTEX_DATA[0]),
-                    gl::STATIC_DRAW);
-
-    gl::UseProgram(program);
-    "out_color".with_c_str(|ptr| gl::BindFragDataLocation(program, 0, ptr));
-
-    // Specify the layout of the vertex data
-    let pos_attr = "position".with_c_str(|ptr| gl::GetAttribLocation(program, ptr) as u32);
-    gl::EnableVertexAttribArray(pos_attr);
-    gl::VertexAttribPointer(pos_attr, 3, gl::FLOAT,
-                            gl::FALSE as GLboolean, 0, ptr::null());
-  }
-
-  app.run(&mut window, &game_iter_settings);
+  App::new().run(&mut window, &GameIteratorSettings {
+    updates_per_second: 30,
+    max_frames_per_second: 30,
+  });
 }
