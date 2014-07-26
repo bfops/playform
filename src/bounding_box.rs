c5f8604d024473cdbbbda422530a1c2721163850
Ben Foppa <benjamin.foppa@gmail.com>
2014-07-25 22:24:13 -0400

    move intersection functions to bounding_box

--- src/bounding_box.rs
+++ src/bounding_box.rs
@@ -1,8 +1,53 @@
 use gl::types::GLfloat;
 use cgmath::vector;
+use cgmath::vector::{Vector3};
 
 #[deriving(Clone)]
 pub struct BoundingBox {
   pub low_corner: vector::Vector3<GLfloat>,
   pub high_corner: vector::Vector3<GLfloat>,
+}
+
+pub type Intersect = Vector3<GLfloat>;
+
+pub enum Intersect1 {
+  Within,
+  Partial,
+}
+
+// Find whether two Blocks intersect.
+pub fn intersect(b1: &BoundingBox, b2: &BoundingBox) -> Option<Intersect> {
+  fn intersect1(x1l: GLfloat, x1h: GLfloat, x2l: GLfloat, x2h: GLfloat) -> Option<Intersect1> {
+    if x1l > x2l && x1h <= x2h {
+      Some(Within)
+    } else if x1h > x2l && x2h > x1l {
+      Some(Partial)
+    } else {
+      None
+    }
+  }
+
+  let mut ret = true;
+  let mut v = Vector3::ident();
+  match intersect1(b1.low_corner.x, b1.high_corner.x, b2.low_corner.x, b2.high_corner.x) {
+    Some(Within) => { },
+    Some(Partial) => { v.x = 0.0; },
+    None => { ret = false; },
+  }
+  match intersect1(b1.low_corner.y, b1.high_corner.y, b2.low_corner.y, b2.high_corner.y) {
+    Some(Within) => { },
+    Some(Partial) => { v.y = 0.0; },
+    None => { ret = false; },
+  }
+  match intersect1(b1.low_corner.z, b1.high_corner.z, b2.low_corner.z, b2.high_corner.z) {
+    Some(Within) => { },
+    Some(Partial) => { v.z = 0.0; },
+    None => { ret = false; },
+  }
+
+  if ret {
+    Some(v)
+  } else {
+    None
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 pub use color::Color4;
-use bounding_box::BoundingBox;
+use bounding_box;
+use bounding_box::{BoundingBox, Intersect};
 use cgmath::aabb::Aabb2;
 use cgmath::angle;
 use cgmath::array::Array2;
@@ -234,50 +235,6 @@ pub struct Block {
   // bounds of the Block
   block_type: BlockType,
   id: u32,
-}
-
-type Intersect = Vector3<GLfloat>;
-
-enum Intersect1 {
-  Within,
-  Partial,
-}
-
-// Find whether two Blocks intersect.
-fn intersect(b1: &BoundingBox, b2: &BoundingBox) -> Option<Intersect> {
-  fn intersect1(x1l: GLfloat, x1h: GLfloat, x2l: GLfloat, x2h: GLfloat) -> Option<Intersect1> {
-    if x1l > x2l && x1h <= x2h {
-      Some(Within)
-    } else if x1h > x2l && x2h > x1l {
-      Some(Partial)
-    } else {
-      None
-    }
-  }
-
-  let mut ret = true;
-  let mut v = Vector3::ident();
-  match intersect1(b1.low_corner.x, b1.high_corner.x, b2.low_corner.x, b2.high_corner.x) {
-    Some(Within) => { },
-    Some(Partial) => { v.x = 0.0; },
-    None => { ret = false; },
-  }
-  match intersect1(b1.low_corner.y, b1.high_corner.y, b2.low_corner.y, b2.high_corner.y) {
-    Some(Within) => { },
-    Some(Partial) => { v.y = 0.0; },
-    None => { ret = false; },
-  }
-  match intersect1(b1.low_corner.z, b1.high_corner.z, b2.low_corner.z, b2.high_corner.z) {
-    Some(Within) => { },
-    Some(Partial) => { v.z = 0.0; },
-    None => { ret = false; },
-  }
-
-  if ret {
-    Some(v)
-  } else {
-    None
-  }
 }
 
 impl Block {
@@ -1017,7 +974,7 @@ impl App {
   /// Find a collision with self.world_data.
   fn world_collision(&self, b: &BoundingBox) -> Option<Intersect> {
     for block in self.world_data.iter() {
-      let i = intersect(b, &block.bounds);
+      let i = bounding_box::intersect(b, &block.bounds);
       match i {
         None => { },
         Some(_) => { return i; },
@@ -1032,7 +989,7 @@ impl App {
       let block = Block::new(low_corner, high_corner, block_type, self.next_block_id);
       let collided = check_collisions &&
             ( self.world_collision(&block.bounds).is_some() || 
-              intersect(&block.bounds, &self.player.bounds).is_some()
+              bounding_box::intersect(&block.bounds, &self.player.bounds).is_some()
             );
 
       if !collided {
@@ -1079,7 +1036,7 @@ impl App {
         .world_data
         .iter()
         .any(|block|
-          match intersect(&new_player_bounds, &block.bounds) {
+          match bounding_box::intersect(&new_player_bounds, &block.bounds) {
             Some(stop) => {
               d_camera_speed = v*stop - v;
               true
