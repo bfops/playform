bebf2a880fb8b37641bd31c8bafc0b666473c68c
Ben Foppa <benjamin.foppa@gmail.com>
2014-12-21 18:37:57 -0500

    stop using the event package's event iterator

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 use common::*;
-use current::Modifier;
+use current::Get;
 use event::WindowSettings;
-use event::{Events, Ups, MaxFps};
-use event_handler::handle_event;
+use interval_timer::IntervalTimer;
+use process_input::process_input;
+use render::render;
 use sdl2_window::*;
 use shader_version::opengl::OpenGL;
 use state::App;
 use stopwatch::TimerSet;
-use std::cell::RefCell;
+use time;
+use update::update;
+use window::{PollEvent, ShouldClose, SwapBuffers};
 use yaglw::gl_context::GLContext;
+
+pub const FRAMES_PER_SECOND: u64 = 30;
+pub const UPDATES_PER_SECOND: u64 = 30;
 
 pub fn main() {
   debug!("starting");
 
-  let window = Sdl2Window::new(
+  let mut window = Sdl2Window::new(
     OpenGL::_3_3,
     WindowSettings {
       title: "playform".to_string(),
@@ -23,7 +29,6 @@ pub fn main() {
       samples: 0,
     }
   );
-  let window = RefCell::new(window);
 
   let (gl, mut gl_context) = unsafe {
     GLContext::new()
@@ -31,15 +36,35 @@ pub fn main() {
 
   let timers = TimerSet::new();
   let mut app = App::new(&gl, &mut gl_context, &timers);
-  let mut game_iter = Events::new(&window);
-  Ups(30).modify(&mut game_iter);
-  MaxFps(30).modify(&mut game_iter);
 
-  loop {
-    match game_iter.next() {
-      None => break,
-      Some(e) => handle_event(&mut app, game_iter.window.borrow_mut().deref_mut(), e)
+  let mut render_timer;
+  let mut update_timer;
+  {
+    let now = time::precise_time_ns();
+    let nanoseconds_per_second = 1000000000;
+    render_timer = IntervalTimer::new(nanoseconds_per_second / FRAMES_PER_SECOND, now);
+    update_timer = IntervalTimer::new(nanoseconds_per_second / UPDATES_PER_SECOND, now);
+  }
+
+  let mut should_close = false;
+  while !should_close {
+    let updates = update_timer.update(time::precise_time_ns());
+    if updates > 0 {
+      update(&mut app);
     }
+
+    let renders = render_timer.update(time::precise_time_ns());
+    if renders > 0 {
+      render(&mut app);
+      window.swap_buffers();
+    }
+
+    while let Some(input) = window.poll_event() {
+      process_input(&mut app, &mut window, input);
+    }
+
+    let ShouldClose(b) = window.get();
+    should_close = b;
   }
 
   debug!("finished!");
--- src/process_input.rs
+++ src/process_input.rs
@@ -1,16 +1,12 @@
 use color::Color4;
 use common::*;
-use event::Event;
-use input;
 use input::{Input,Button,Motion};
-use input::keyboard::Key;
+use input::{keyboard, mouse};
 use nalgebra::Vec3;
-use render::render;
+use sdl2;
 use sdl2_window::*;
-use sdl2::mouse;
 use state::App;
 use std::f32::consts::PI;
-use update::update;
 use vertex::ColoredVertex;
 
 #[inline]
@@ -21,52 +17,48 @@ fn swap_remove_first<T: PartialEq + Copy>(v: &mut Vec<T>, t: T) {
   }
 }
 
-pub fn handle_event<'a>(app: &mut App<'a>, game_window: &mut Sdl2Window, event: Event) {
-  match event {
-    Event::Render(_) => render(app),
-    Event::Update(_) => update(app),
-    Event::Input(ref i) => match *i {
-      Input::Press(Button::Keyboard(key)) => key_press(app, key),
-      Input::Release(Button::Keyboard(key)) => key_release(app, key),
-      Input::Press(Button::Mouse(button)) => mouse_press(app, button),
-      Input::Release(Button::Mouse(button)) => mouse_release(app, button),
-      Input::Move(Motion::MouseCursor(x, y)) => mouse_move(app, game_window, x, y),
-      _ => {},
-    },
+pub fn process_input<'a>(app: &mut App<'a>, game_window: &mut Sdl2Window, input: Input) {
+  match input {
+    Input::Press(Button::Keyboard(key)) => key_press(app, key),
+    Input::Release(Button::Keyboard(key)) => key_release(app, key),
+    Input::Press(Button::Mouse(button)) => mouse_press(app, button),
+    Input::Release(Button::Mouse(button)) => mouse_release(app, button),
+    Input::Move(Motion::MouseCursor(x, y)) => mouse_move(app, game_window, x, y),
+    _ => {},
   }
 }
 
-fn key_press<'a>(app: &mut App<'a>, key: input::keyboard::Key) {
+fn key_press<'a>(app: &mut App<'a>, key: keyboard::Key) {
   app.timers.time("event.key_press", || {
     match key {
-      Key::A => {
+      keyboard::Key::A => {
         app.player.walk(Vec3::new(-1.0, 0.0, 0.0));
       },
-      Key::D => {
+      keyboard::Key::D => {
         app.player.walk(Vec3::new(1.0, 0.0, 0.0));
       },
-      Key::Space => {
+      keyboard::Key::Space => {
         if !app.player.is_jumping {
           app.player.is_jumping = true;
           // this 0.3 is duplicated in a few places
           app.player.accel.y = app.player.accel.y + 0.3;
         }
       },
-      Key::W => {
+      keyboard::Key::W => {
         app.player.walk(Vec3::new(0.0, 0.0, -1.0));
       },
-      Key::S => {
+      keyboard::Key::S => {
         app.player.walk(Vec3::new(0.0, 0.0, 1.0));
       },
-      Key::Left =>
+      keyboard::Key::Left =>
         app.player.rotate_lateral(PI / 12.0),
-      Key::Right =>
+      keyboard::Key::Right =>
         app.player.rotate_lateral(-PI / 12.0),
-      Key::Up =>
+      keyboard::Key::Up =>
         app.player.rotate_vertical(PI / 12.0),
-      Key::Down =>
+      keyboard::Key::Down =>
         app.player.rotate_vertical(-PI / 12.0),
-      Key::M => {
+      keyboard::Key::M => {
         let updates = [
           ColoredVertex {
             position: app.player.camera.position,
@@ -79,10 +71,10 @@ fn key_press<'a>(app: &mut App<'a>, key: input::keyboard::Key) {
         ];
         app.line_of_sight.buffer.update(app.gl_context, 0, &updates);
       },
-      Key::O => {
+      keyboard::Key::O => {
         app.render_octree = !app.render_octree;
       }
-      Key::L => {
+      keyboard::Key::L => {
         app.render_outlines = !app.render_outlines;
       }
       _ => {},
@@ -90,27 +82,27 @@ fn key_press<'a>(app: &mut App<'a>, key: input::keyboard::Key) {
   })
 }
 
-fn key_release<'a>(app: &mut App<'a>, key: Key) {
+fn key_release<'a>(app: &mut App<'a>, key: keyboard::Key) {
   app.timers.time("event.key_release", || {
     match key {
       // accelerations are negated from those in key_press.
-      Key::A => {
+      keyboard::Key::A => {
         app.player.walk(Vec3::new(1.0, 0.0, 0.0));
       },
-      Key::D => {
+      keyboard::Key::D => {
         app.player.walk(Vec3::new(-1.0, 0.0, 0.0));
       },
-      Key::Space => {
+      keyboard::Key::Space => {
         if app.player.is_jumping {
           app.player.is_jumping = false;
           // this 0.3 is duplicated in a few places
           app.player.accel.y = app.player.accel.y - 0.3;
         }
       },
-      Key::W => {
+      keyboard::Key::W => {
         app.player.walk(Vec3::new(0.0, 0.0, 1.0));
       },
-      Key::S => {
+      keyboard::Key::S => {
         app.player.walk(Vec3::new(0.0, 0.0, -1.0));
       },
       _ => { }
@@ -129,7 +121,7 @@ fn mouse_move<'a>(app: &mut App<'a>, w: &mut Sdl2Window, x: f64, y: f64) {
     app.player.rotate_lateral(rx);
     app.player.rotate_vertical(ry);
 
-    mouse::warp_mouse_in_window(
+    sdl2::mouse::warp_mouse_in_window(
       &w.window,
       WINDOW_WIDTH as i32 / 2,
       WINDOW_HEIGHT as i32 / 2
@@ -137,12 +129,12 @@ fn mouse_move<'a>(app: &mut App<'a>, w: &mut Sdl2Window, x: f64, y: f64) {
   })
 }
 
-fn mouse_press<'a>(app: &mut App<'a>, button: input::mouse::MouseButton) {
+fn mouse_press<'a>(app: &mut App<'a>, button: mouse::MouseButton) {
   app.timers.time("event.mouse_press", || {
     app.mouse_buttons_pressed.push(button);
   })
 }
 
-fn mouse_release<'a>(app: &mut App<'a>, button: input::mouse::MouseButton) {
+fn mouse_release<'a>(app: &mut App<'a>, button: mouse::MouseButton) {
   swap_remove_first(&mut app.mouse_buttons_pressed, button)
 }
