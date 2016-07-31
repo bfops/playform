1208c1840c9c522e24ba23cc9cfe1200fe35543d
Zeke Foppa <benjamin.foppa@gmail.com>
2016-07-31 01:48:29 -0400

    wind and grass grows in the distance

--- client/lib/src/view/shaders/grass_billboard.rs
+++ client/lib/src/view/shaders/grass_billboard.rs
@@ -20,6 +20,7 @@ pub fn new<'a, 'b:'a>(gl: &'a GLContext, near: f32, far: f32) -> T<'b> {
 
       uniform mat4 projection_matrix;
       uniform vec3 eye_position;
+      uniform float time_ms;
 
       uniform samplerBuffer positions;
       uniform samplerBuffer normals;
@@ -32,6 +33,9 @@ pub fn new<'a, 'b:'a>(gl: &'a GLContext, near: f32, far: f32) -> T<'b> {
       out vec2 vs_texture_position;
       out vec3 vs_normal;
       out float vs_tex_id;
+
+      // include cnoise
+      {}
 
       // include adjust_depth_precision
       {}
@@ -49,24 +53,31 @@ pub fn new<'a, 'b:'a>(gl: &'a GLContext, near: f32, far: f32) -> T<'b> {
         return mat3(1) + skew + skew*skew*(1-c)/(s*s);
       }}
 
-      // Barycentric interpolation function to smoothly interpolate vertex-associated values over a triangle.
-      // This returns the weight that should be given to each vertex value at a specified point.
-      vec3 barycentric(mat3 v, vec3 p) {{
-        mat3 d = v - mat3(p, p, p);
-        vec3 weights =
-          vec3(
-            length(cross(d[1], d[2])),
-            length(cross(d[2], d[0])),
-            length(cross(d[0], d[1]))
-          );
-        return weights / dot(weights, vec3(1));
-      }}
-
       vec3 vec3Fetch(samplerBuffer vs, int float_idx) {{
         vec3 r;
         r.x = texelFetch(vs, float_idx + 0).r;
         r.y = texelFetch(vs, float_idx + 1).r;
         r.z = texelFetch(vs, float_idx + 2).r;
+        return r;
+      }}
+
+      // compute a model-space shear to transform (0,1,0) to a desired vector.
+      mat4 shearTo(vec3 desired) {{
+        mat4 r = mat4(1);
+
+        float d = dot(desired, vec3(0, 1, 0));
+        // tweak the dot product to be in a valid range
+        float new_d = exp(d - 1.0);
+        // the common shear factor
+        float k = 0;
+        if (d < 0.99) {{
+          k = sqrt((1.0-new_d*new_d)/(new_d*new_d*(1.0-d*d)));
+        }}
+
+        r[1].x = desired.x * k;
+        r[1].y = 1.0;
+        r[1].z = desired.z * k;
+
         return r;
       }}
 
@@ -93,7 +104,7 @@ pub fn new<'a, 'b:'a>(gl: &'a GLContext, near: f32, far: f32) -> T<'b> {
             vec3Fetch(normals, normal_id + 3),
             vec3Fetch(normals, normal_id + 6)
           );
-        vec3 normal = vertex_normals * barycentric(vertices, root);
+        vec3 normal = vertex_normals * vec3(1.0/3.0);
 
         mat4 translation = mat4(1.0);
         translation[3].xyz = root;
@@ -101,34 +112,27 @@ pub fn new<'a, 'b:'a>(gl: &'a GLContext, near: f32, far: f32) -> T<'b> {
         mat3 rotate_normal = between(vec3(0, 1, 0), normal);
         mat4 rotation = mat4(rotate_normal);
 
-        // We will apply a model-space skew transformation to make the grass point
-        // in a desired direction. In model coordinates the grass initially points
-        // to (0, 1, 0), so to skew this into (xd, yd, zd) we apply a skew of
-        //    x' = x0 + xd*(y/yd)
-        //    y' = y
-        //    z' = z0 + zd*(y/yd)
-        mat4 skew = mat4(1.0);
-        {{
-          vec3 desired = vec3(0, 1, 0);
-          float d = dot(normal, desired);
-          if (d < 0.99) {{
-            // in model space
-            vec3 desired = inverse(rotate_normal) * desired;
-            // tweak the dot product to be in a valid range
-            float new_d = exp(d - 1.0);
-            // the common skew factor
-            float k = sqrt((1.0-new_d*new_d)/(new_d*new_d*(1.0-d*d)));
+        mat4 shear = shearTo(inverse(rotate_normal) * vec3(0, 1, 0));
 
-            skew[1].x = desired.x * k;
-            skew[1].y = 1.0;
-            skew[1].z = desired.z * k;
-          }}
-        }};
+        mat4 noise_shear;
+        {{
+          float azimuth = 3.14 * cnoise(root + vec3(122, -1, 14.5) + vec3(0, time_ms / 2000, 0));
+          float altitude = 3.14/2 - 3.14/4 * (cnoise(root + vec3(-18.11, 101.1, 44.5) + vec3(0, time_ms / 2000, 0)) + 1) / 2.0;
+          vec3 v =
+            vec3(
+              cos(altitude) * cos(azimuth),
+              sin(altitude),
+              cos(altitude) * sin(azimuth)
+            );
+          noise_shear = shearTo(v);
+        }}
 
         mat4 scale = mat4(1.0);
-        scale[0].x = scale[2].z = 4.0;
+        float distance_scaling = exp(length(root - eye_position) / (1 << 6));
+        scale[1].y = distance_scaling * 0.4;
+        scale[0].x = scale[2].z = distance_scaling * 4.0;
 
-        mat4 model_matrix = translation * rotation * skew * scale;
+        mat4 model_matrix = translation * rotation * shear * noise_shear * scale;
 
         gl_Position =
           adjust_depth_precision(
@@ -140,6 +144,7 @@ pub fn new<'a, 'b:'a>(gl: &'a GLContext, near: f32, far: f32) -> T<'b> {
         vs_normal = normal;
       }}"#,
       shaders::adjust_depth_precision::as_string(near, far),
+      shaders::noise::cnoise(),
     )),
     (gl::FRAGMENT_SHADER, format!("
       #version 330 core
