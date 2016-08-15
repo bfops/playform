31e017c46be7e7f706e751b2dfcc4307790bc174
Zeke Foppa <benjamin.foppa@gmail.com>
2016-08-14 20:27:28 -0400

    foo

--- scattering.m
+++ scattering.m
@@ -1,53 +1,109 @@
-k = 1;
-r_atmos = 4*k;
-max_h = r_atmos * 2;
+global planet_scale = 1000;
+global planet_radius = 6400;
 
-cols = 100;
-rows = 21;
-hs = max_h*[1:cols]/cols;
-h = repmat(hs, rows, 1);
-ys = (2.*[0:rows-1]'./(rows-1)-1)*3.14/2;
-y = repmat(ys, 1, cols);
-s = sin(y);
-c = cos(y);
+global planet_center_x = 0;
+global planet_center_y = -planet_radius;
 
-sun_x = 1000;
-sun_y = h;
+global atmos_thickness_ratio = 0.025;
+global atmos_thickness = planet_radius * atmos_thickness_ratio;
 
-r1 = repmat(0, rows, cols);
-step = 10;
-max_t = 4*max_h;
-for t = [1:max_t*step]/step
-  point_x = t .* c;
-  point_y = h + t .* s;
+global scale_height = atmos_thickness / 4;
 
-  l = sqrt(point_x.*point_x + point_y._point_y);
+function r = vec_len(d_x, d_y)
+  r = sqrt(d_x .* d_x + d_y .* d_y);
+endfunction
 
-  step2 = 10
-  for t2 = [1:step2]/step2
-    point2_x = t2 .* c;
-    point2_y = h + t2 .* s;
-    l2 = sqrt(point2_x.*point2_x + point2_y.*point2_y);
-    r1 += exp(-k .* (l + l2));
-  end
+function r = atmos_density (p_x, p_y)
+  global planet_center_x;
+  global planet_center_y;
+  global planet_radius;
+  global scale_height;
+  d_x = p_x - planet_center_x;
+  d_y = p_y - planet_center_y;
+  dist_from_surface = vec_len(d_x, d_y) - planet_radius;
+  r = exp(-dist_from_surface / scale_height);
+endfunction
 
-  d_x = sun_x - point_x;
-  d_y = sun_y - point_y;
-  l = sqrt(d_x.*d_x + d_y.*d_y);
+function r = optical_depth(a_x, a_y, b_x, b_y)
+  global atmos_thickness;
+  global planet_radius;
+  samples = 5;
+  d_x = b_x - a_x;
+  d_y = b_y - a_y;
+  l = vec_len(d_x, d_y);
   d_x = d_x ./ l;
   d_y = d_y ./ l;
-  for t2 = [1:max_t*step]/step
-    point2_x = point_x + t2 .* d_x;
-    point2_y = point_y + t2 .* d_y;
-    l2 = sqrt(point_x.*point_x + point_y.*point_y);
-    r1 += exp(-k .* l2);
-  end
-endfor
+  l = min(l, 2 * (atmos_thickness + planet_radius)) / samples;
+  d_x = d_x .* l;
+  d_y = d_y .* l;
+
+  r = 0;
+  for i = [1:samples]
+    p_x = a_x + i .* d_x;
+    p_y = a_y + i .* d_y;
+    r = r + atmos_density(p_x, p_y) .* l;
+  endfor
+endfunction
+
+function r = phase(cos_angle, g)
+  c = cos_angle;
+  g2 = g .* g;
+  r = 3 * (1-g2) .* (1 + c.*c) ./ (2 * (2 + g2) .* realpow(1 + g2 - 2*g.*c, 3.0 / 2.0));
+endfunction
+
+function r = dot(x1, y1, x2, y2)
+  r = x1.*x2 + y1.*y2;
+endfunction
+
+function r = in_scatter(sun_angle, camera_x, camera_y, look_x, look_y, k, g)
+  global planet_center_x;
+  global planet_center_y;
+  global planet_radius;
+  global atmos_thickness;
+  sun_distance = 150000000;
+  sun_position_x = planet_center_x + cos(sun_angle) .* sun_distance;
+  sun_position_y = planet_center_y + sin(sun_angle) .* sun_distance;
+
+  samples = 5;
+  l = atmos_thickness / samples;
+  r = 0;
+  for i = [1:samples]
+    point_x = camera_x + look_x * i * l;
+    point_y = camera_y + look_y * i * l;
+    d1_x = sun_position_x - point_x;
+    d1_y = sun_position_y - point_y;
+    d2_x = point_x - camera_x;
+    d2_y = point_y - camera_y;
+    cos_angle = dot(d1_x, d1_y, d2_x, d2_y) ./ (vec_len(d1_x, d1_y) .* vec_len(d2_x, d2_y));
+    od = optical_depth(camera_x, camera_y, point_x, point_y) + optical_depth(point_x, point_y, sun_position_x, sun_position_y);
+    r += k .* phase(cos_angle, g) .* atmos_density(point_x, point_y) .* exp(-k .* od) * l;
+  endfor
+endfunction
+
+camera_x = 0;
+camera_y = 0;
+
+max_k = 1;
+k_samples = 1000;
+k = [1:max_k*k_samples]/k_samples;
+
+noon_up = in_scatter(3.14/2, camera_x, camera_y, 0, 1, k, 0);
+noon_horizon = in_scatter(3.14/2, camera_x, camera_y, 1, 0, k, 0);
+sunset_up = in_scatter(0, camera_x, camera_y, 0, 1, k, 0);
+sunset_horizon = in_scatter(0, camera_x, camera_y, 1, 0, k, 0);
 
 figure 1;
-plot(hs, r1);
-axis([0 max_h 0 2]);
+plot(k, [noon_up]);
+axis([0 1 0 1]);
 
 figure 2;
-%plot(ys, r1');
-axis([-3.14/2 3.14/2 0 2]);
+plot(k, [noon_horizon]);
+axis([0 1 0 1]);
+
+figure 3;
+plot(k, [sunset_up]);
+axis([0 1 0 1]);
+
+figure 4;
+plot(k, [sunset_horizon]);
+axis([0 1 0 1]);
