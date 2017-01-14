3d874bbd7d60efc598055911a7db33b00ad44ea6
Zeke Foppa <benjamin.foppa@gmail.com>
2017-01-14 17:07:19 -0500

    rename

--- server/lib/src/terrain_loader.rs
+++ server/lib/src/terrain_loader.rs
@@ -41,19 +41,19 @@ impl T {
 
   pub fn load<LoadBlock>(
     &self,
-    id_allocator   : &Mutex<id_allocator::T<entity_id::T>>,
-    physics        : &Mutex<Physics>,
-    block_position : &voxel::bounds::T,
-    new_lod        : lod::T,
-    owner          : lod::OwnerId,
-    load_block     : &mut LoadBlock,
+    id_allocator : &Mutex<id_allocator::T<entity_id::T>>,
+    physics      : &Mutex<Physics>,
+    position     : &voxel::bounds::T,
+    new_lod      : lod::T,
+    owner        : lod::OwnerId,
+    load_block   : &mut LoadBlock,
   ) where LoadBlock: FnMut(update_gaia::Message)
   {
     let prev_lod;
     let max_lod_changed: bool;
     let mut lod_map = self.lod_map.lock().unwrap();
     let mut in_progress_terrain = self.in_progress_terrain.lock().unwrap();
-    match lod_map.get(block_position, owner) {
+    match lod_map.get(position, owner) {
       Some((Some(prev), lods)) => {
         prev_lod = Some(prev);
         if new_lod == prev {
@@ -78,25 +78,25 @@ impl T {
 
     if !max_lod_changed {
       // Maximum lod::T is unchanged.
-      let (_, change) = lod_map.insert(*block_position, new_lod, owner);
+      let (_, change) = lod_map.insert(*position, new_lod, owner);
       assert!(change.is_none());
       return;
     }
 
     match new_lod {
       lod::Placeholder => {
-        let (_, change) = lod_map.insert(*block_position, new_lod, owner);
+        let (_, change) = lod_map.insert(*position, new_lod, owner);
         let change = change.unwrap();
         assert!(change.loaded == None);
         assert!(prev_lod == None);
         assert!(change.desired == Some(lod::Placeholder));
-        in_progress_terrain.insert(id_allocator, physics, block_position);
+        in_progress_terrain.insert(id_allocator, physics, position);
       },
       lod::Full => {
         let mut generate_block = || {
-          debug!("{:?} requested from gaia", block_position);
+          debug!("{:?} requested from gaia", position);
           load_block(
-            update_gaia::Message::Load(time::precise_time_ns(), vec!(*block_position), LoadReason::Local(owner))
+            update_gaia::Message::Load(time::precise_time_ns(), vec!(*position), LoadReason::Local(owner))
           );
         };
         generate_block();
@@ -150,12 +150,12 @@ impl T {
 
   pub fn unload(
     &self,
-    physics        : &Mutex<Physics>,
-    block_position : &voxel::bounds::T,
-    owner          : lod::OwnerId,
+    physics  : &Mutex<Physics>,
+    position : &voxel::bounds::T,
+    owner    : lod::OwnerId,
   ) {
     let lod_change;
-    match self.lod_map.lock().unwrap().remove(*block_position, owner) {
+    match self.lod_map.lock().unwrap().remove(*position, owner) {
       (_, None) => return,
       (_, Some(c)) => lod_change = c,
     }
@@ -163,11 +163,11 @@ impl T {
     lod_change.loaded.map(|loaded_lod| {
       match loaded_lod {
         lod::Placeholder => {
-          self.in_progress_terrain.lock().unwrap().remove(physics, block_position);
+          self.in_progress_terrain.lock().unwrap().remove(physics, position);
         }
         lod::Full => {
           stopwatch::time("terrain_loader.unload", || {
-            match self.loaded.lock().unwrap().get(block_position) {
+            match self.loaded.lock().unwrap().get(position) {
               None => {
                 // Unloaded before the load request completed.
               },
